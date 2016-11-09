0fc4fe59d961cb43850d1534951dca9616b99500
capm4 <kruglov.aleksandr@gmail.com>
2016-11-09 20:22:57 +0200

    HomeWork_3_1 for Check

---
+++ src/Module3/HW/HomeWork_3_1/Bird.java
@@ -0,0 +1,15 @@
+package Module3.HW.HomeWork_3_1;
+
+public class Bird {
+    public static void main(String[] args) {
+        sing("I am walking");
+        sing("I am flying");
+        sing("I am singing");
+        sing("I am Bird");
+    }
+
+    private static void sing(String singS){
+        System.out.println(singS);
+    }
+
+}
