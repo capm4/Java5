eb59cbf37d71e6671bb72332cdff7952add61985
capm4 <kruglov.aleksandr@gmail.com>
2016-11-09 20:44:19 +0200

    HomeWork_3_2 with corrections for Sum

---
+++ src/Module3/HW/HomeWork_3_2/Adder.java
@@ -0,0 +1,11 @@
+package Module3.HW.HomeWork_3_2;
+
+public class Adder extends Arithmetic {
+    public static void main(String[] args) {
+        System.out.println(check (2,2));
+    }
+
+    public static boolean check(Integer a, Integer b){
+        return a.equals(b);
+    }
+ }
---
+++ src/Module3/HW/HomeWork_3_2/Arithmetic.java
@@ -0,0 +1,14 @@
+package Module3.HW.HomeWork_3_2;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        add(1, 2);
+
+    }
+
+     public static int add(Integer a, Integer b){
+        int resuld = a + b;
+        return resuld;
+     }
+
+}
