7086770ff721df3328ff3687956f19fa48d52c51
capm4 <kruglov.aleksandr@gmail.com>
2016-11-23 21:12:45 +0200

    HomeWork_4_4 check metot with private

--- src/Module4/HW/HomeWork_4_4/BankSystemImpl.java
+++ src/Module4/HW/HomeWork_4_4/BankSystemImpl.java
@@ -4,24 +4,30 @@ package Module4.HW.HomeWork_4_4;
  * Created by user on 18.11.2016.
  */
 public class BankSystemImpl implements BankSystem {
+
     @Override
     public void withdrawOfUser(User user, int amount) {
-        user.balance-= amount;
+        double balance = user.getBalance();
+        user.setBalance(balance-=amount);
     }
 
     @Override
     public void fundUser(User user, int amount) {
-        user.balance+=amount;
+        double balance = user.getBalance();
+        user.setBalance(balance+=amount);
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-        fromUser.balance -= amount;
-        toUser.balance += amount;
+        double balanceFrom = fromUser.getBalance();
+        fromUser.setBalance(balanceFrom-=amount);
+        double balanceTo = toUser.getBalance();
+        toUser.setBalance(balanceTo-=amount);
     }
 
     @Override
     public void paySalary(User user) {
-        user.balance+=user.salary;
+        double balance = user.getBalance();
+        user.setBalance(balance+=user.getSalary());
     }
 }
