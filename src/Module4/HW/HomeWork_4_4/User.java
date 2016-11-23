0f0f9ad16c95eac9452745ca877a0e87db12564c
capm4 <kruglov.aleksandr@gmail.com>
2016-11-23 20:42:56 +0200

    HomeWork_4_6 Users filds private, add getters and setter

--- src/Module4/HW/HomeWork_4_4/User.java
+++ src/Module4/HW/HomeWork_4_4/User.java
@@ -4,11 +4,67 @@ package Module4.HW.HomeWork_4_4;
 import Module4.HW.HomeWork_4_1.Bank;
 
 public class User {
-    protected long id;
-    protected String name;
-    protected double balance;
-    protected int monthsOfEmployment;
-    protected String companyName;
-    protected int salary;
-    protected Bank bank;
+    private long id;
+    private String name;
+    private double balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private Bank bank;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
 }
