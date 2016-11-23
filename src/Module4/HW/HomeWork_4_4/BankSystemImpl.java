c23f1aa5e9656727454c77a96bb6c2cdc3811ebb
capm4 <kruglov.aleksandr@gmail.com>
2016-11-23 20:38:09 +0200

    HomeWork_4_4 BankSystemImpl with realization

--- src/Module4/HW/HomeWork_4_4/BankSystemImpl.java
+++ src/Module4/HW/HomeWork_4_4/BankSystemImpl.java
@@ -6,21 +6,22 @@ package Module4.HW.HomeWork_4_4;
 public class BankSystemImpl implements BankSystem {
     @Override
     public void withdrawOfUser(User user, int amount) {
-
+        user.balance-= amount;
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
+        user.balance+=amount;
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-
+        fromUser.balance -= amount;
+        toUser.balance += amount;
     }
 
     @Override
     public void paySalary(User user) {
-
+        user.balance+=user.salary;
     }
 }
