278a6391320dcf32caf357b03c4d6fa5de79cdda
capm4 <kruglov.aleksandr@gmail.com>
2016-12-25 08:48:03 +0200

    HomeWork_9 with static fix

--- src/Module9/HM/HomeWork_9/ArraysUtils.java
+++ src/Module9/HM/HomeWork_9/ArraysUtils.java
@@ -14,13 +14,13 @@ import static Module9.HM.HomeWork_9.Currency.USD;
  * Created by user on 22.12.2016.
  */
 public class ArraysUtils {
-    public List<Order> sortByPrizeDegres (List<Order> a){
+    public static List<Order> sortByPrizeDegres (List<Order> a){
         a.sort(Comparator.comparing(Order :: getPrice));
         return a;
     }
 
     //отсортируйте список за ценой заказа по возрастанию и за городом пользователя
-    public List<Order> sortByPrizeAndCity (List<Order> a){
+    public static List<Order> sortByPrizeAndCity (List<Order> a){
         a.sort(Comparator.comparing(Order :: getPrice)
                 .reversed()
                 .thenComparing((order1, order2) -> order1.getUser().getCity().compareTo(order2.getUser().getCity())));
@@ -29,7 +29,7 @@ public class ArraysUtils {
 
     //отсортируйте список за наименованием товара, идентификатором заказа, и городом пользователя
 
-    public List<Order> sortByItemIdenAndCity(List<Order> a){
+    public static List<Order> sortByItemIdenAndCity(List<Order> a){
         a.sort(Comparator.comparing(Order :: getItemName)
                 .thenComparing(Order :: getShopIdentificator)
                 .thenComparing((order1, order2) -> order1.getUser().getCity().compareTo(order2.getUser().getCity())));
@@ -37,21 +37,21 @@ public class ArraysUtils {
     }
 
     //удалите дублированные данные со списка
-    public List<Order> delDublicate (List<Order> a){
+    public static List<Order> delDublicate (List<Order> a){
         a.stream()
         .distinct().forEach((p)-> System.out.println(p));
         return a;
     }
 
     //удалите объекты, где цена меньше 1500
-    public  List<Order> delPrize1500 (List<Order> a){
+    public  static  List<Order> delPrize1500 (List<Order> a){
         a.removeIf((p) -> p.getPrice() == 1500);
         a.stream().forEach((p)-> System.out.println(p));
         return  a;
     }
 
     //разделите список на 2 списка - заказы в долларах и в гривнах
-    public  List<Order> sortUSDandUAH(List<Order> a){
+    public static   List<Order> sortUSDandUAH(List<Order> a){
         List<Order> usdList = a.stream()
                 .filter((p) -> p.getCurrency().equals(USD))
                 .collect(Collectors.toList());
@@ -66,7 +66,7 @@ public class ArraysUtils {
     }
 
     //разделите список на столько списков, сколько уникальных городов в User
-    public List<Order> listOfCity (List<Order> a){
+    public static List<Order> listOfCity (List<Order> a){
         Map<String, List<Order>> ordersByCities = a.stream()
                 .collect(Collectors.groupingBy(order -> order.getUser().getCity()));
         System.out.println(ordersByCities.get("LA"));
@@ -77,7 +77,7 @@ public class ArraysUtils {
     }
 
     //проверьте, содержит ли сет заказ, где фамилия пользователя - “Petrov”
-    public List<Order> orderPetrov (List<Order> a){
+    public static List<Order> orderPetrov (List<Order> a){
         List<Order> Pertov = a.stream()
                 .filter((p)->p.getUser().getLastName().equals("Petrov"))
                 .collect(Collectors.toList());
@@ -87,7 +87,7 @@ public class ArraysUtils {
     }
 
     //удалите заказы в USD
-    public  List<Order> delUSD (List<Order> a) {
+    public static List<Order> delUSD (List<Order> a) {
         a.removeIf((p) -> p.getCurrency().equals(USD));
         a.forEach((p) -> System.out.println(p));
         return a;
